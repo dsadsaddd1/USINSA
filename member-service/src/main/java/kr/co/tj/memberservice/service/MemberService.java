59a6f130c38f438fdcddb864013a7c57ae67403d
tj705 <cay0209@gmail.com>
2023-05-15 15:14:53 +0900

    member-service 기본작업

---
+++ member-service/src/main/java/kr/co/tj/memberservice/service/MemberService.java
@@ -0,0 +1,14 @@
+package kr.co.tj.memberservice.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import kr.co.tj.memberservice.repository.MemberRepository;
+
+@Service
+public class MemberService {
+
+	@Autowired
+	private MemberRepository memberRepository;
+// // // 	
+}
